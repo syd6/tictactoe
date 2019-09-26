558f7164cb472f87a0b3b66172f5b0be114524ce
syd6 <syd6@cornell.edu>
2019-09-26 15:08:56 -0400

    win logic is wrong

--- gamelogic.js
+++ gamelogic.js
@@ -10,7 +10,7 @@ var winCombo8 = ["1-3", "2-2", "3-1"];
 var allwins = [winCombo1, winCombo2, winCombo3, winCombo4, winCombo5, winCombo6, winCombo7, winCombo8];
 
 var player1;
-var player2
+var player2;
 
 class Player {
     constructor(name) {
@@ -24,11 +24,14 @@ function gameIntro(){
     player1 = new Player(document.getElementById("player-one").value);
     player2 = new Player(document.getElementById("player-two").value);
     var welcomeMessage = player1.name+" (blue) & "+player2.name+" (red), welcome to Tic-Tac-Toe"
-    var scoreBoard = [player1.score, player2.score]
     document.getElementById("welcome-message").innerHTML = welcomeMessage;
-    document.getElementById("scoreboard").innerHTML = scoreBoard;
+    updateScore();
     document.getElementById("tic-tac-toe").style.display = "grid";
-    document.getElementById("welcome-image").style.display = "block";
+}
+
+function updateScore(){
+    var scoreBoard = [player1.score, player2.score]
+    document.getElementById("scoreboard").innerHTML = "SCOREBOARD: "+scoreBoard;
 }
 
 function detectMove(id){
@@ -36,8 +39,9 @@ function detectMove(id){
     if (moveTracker % 2 != 0){// if odd
         //notes: player1 is always gonna have the odd moves. If move #  % 2 is not =  0 assign that move to player1
         var example = document.getElementById(id);
-        example.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+        example.style.backgroundColor = "blue";
         player1.moves.push(id)
+        //if (player1.moves.length >= 3) detectWin(player1)
         detectWin(player1)
     }
     else{ 
@@ -45,15 +49,33 @@ function detectMove(id){
         var example = document.getElementById(id);
         example.style.backgroundColor = "red";
         player2.moves.push(id)
-        detectWin(player2)
+        //if (player2.moves.length >= 3) detectWin(player2)
+       detectWin(player2)
     }
 }
 
 function detectWin(player){
-
     for (i = 0; i < allwins.length; i++){
-        if (JSON.stringify(player.moves) == JSON.stringify(allwins[i])){ //has to be contain cos does the order matter
+        /*for (move in player.moves){
+            if (!allwins[i].includes(move)) return;
+        }
+        document.getElementById("winner").innerHTML = player.name+" wins"
+        return*/
+        if (JSON.stringify(player.moves) == JSON.stringify(allwins[i])){ //doesn't work if the cells are inputted out of order
+            //if each thing in 
             document.getElementById("winner").innerHTML = player1.name+" wins"
+            player.score += 1;
+            updateScore();
+            document.getElementById("new-game").style.display = "block"
         }
     }
 }
+
+function clearBoard(){
+    var squares = document.getElementsByClassName("tic-tac-toe")
+    for (square in squares){
+        square.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+    }
+
+}
+
